70099b0a72bc7f8769c6970e3247db360ad17cc4
LeonardoRibeiro <lr20250523@gmail.com>
2024-06-10 10:13:47 -0300

    Criptografia para arquivo de texto adicionada

--- crip-simetrica/src/text/TextEncoding.java
+++ crip-simetrica/src/text/TextEncoding.java
@@ -1,7 +1,32 @@
 package text;
 
+import core.Crypt;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import utils.BlocksConvert;
+import utils.FileManipulation;
+import utils.RoundKeyGenerate;
+
 public class TextEncoding {
-    public static void textEncoding(String initArq, String destArq, byte [][] key){
-        
+
+    public static void textEncoding(String initFile, String destFile, byte [][] key){
+        String [] arqInitLines = FileManipulation.readFileAsLines(initFile);
+        if(arqInitLines == null){
+            return;
+        }
+
+        byte [][][] keysRound = RoundKeyGenerate.roundKeyGenerate(key);
+
+        for(String line : arqInitLines){
+            byte [][][] blocks = BlocksConvert.blocksConverter(line);
+            byte [][][] blocksCript = Crypt.criptBlocks(blocks, keysRound);
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(destFile, true))) {
+                FileManipulation.writeByteMatrixToFile(blocksCript, writer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ crip-simetrica/src/utils/FileManipulation.java
@@ -0,0 +1,44 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManipulation {
+
+    public static String[] readFileAsLines(String filePath) {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return lines.toArray(new String[0]);
+    }
+
+    public static void writeByteMatrixToFile(byte[][][] data, BufferedWriter writer) {
+        try {
+            for (byte[][] matrix : data) {
+                for (byte[] row : matrix) {
+                    for (byte b : row) {
+                        writer.write(String.format("%02x", b));
+                        writer.write(" ");  
+                    }
+                    writer.newLine();  
+                }
+                writer.newLine();  
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
